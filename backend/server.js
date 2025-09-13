ba36df589f595fd66a509d7334c3f718b3c4e9a5
Dhruv Agrawal <dhruvagrawal963@gmail.com>
2025-09-14 00:13:44 +0530

    Setup

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+const app=express();
+
+app.get('/api/auth/signup',(req,res)=>{
+    res.send("Signup endpoint");
+})
+app.get('/api/auth/login',(req,res)=>{
+    res.send('Login endpoint');
+});
+app.get('api/auth/logout',(req,res)=>{
+    res.send('Logout endpoint');
+});
+
+app.listen(3000,()=>{
+    console.log(`Server is running on http://localhost:3000`);
+});
