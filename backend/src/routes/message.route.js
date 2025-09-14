efb03138b41a2b028326bcd778520b8b40a3e8fb
Dhruv Agrawal <dhruvagrawal963@gmail.com>
2025-09-14 15:37:51 +0530

    deploy

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,6 @@
+import express from 'express';
+const router=express.Router();
+router.get('/send',(req,res)=>{
+    res.send('Send message endpoint');
+})
+export default router;
---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const app = express();
+const __dirname = path.resolve();
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
